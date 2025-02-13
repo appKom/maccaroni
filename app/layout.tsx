2d21b0dcc1eef132bf5df7931e988b1f6b7a9beb
fredrir <fhansteen@gmail.com>
2025-02-13 20:48:26 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Online Veldedighetsauksjon",
+  title: "Onlove",
   description: "Veldedighetsauksjon for Online Linjeforening",
 };
 
