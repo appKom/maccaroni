01a6fb91662cce1d5247364daf9b7fb0502933d6
fredrir <fhansteen@gmail.com>
2025-02-13 13:16:35 +0100

    setup up SessionWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-"use client";
-
-import { ReactNode } from "react";
-import { SessionProvider, signIn, useSession } from "next-auth/react";
-import { Toaster } from "react-hot-toast";
-import { Button } from "@/components/Button";
 import { Metadata } from "next";
+import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/auth/SessionWrapper";
+import RootLayout from "@/components/auth/RootLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,57 +15,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-const RootLayout = ({ children }: RootLayoutProps) => {
-  const { data: session, status } = useSession();
-
-  const handleLogin = () =>
-    signIn("auth0", {
-      callbackUrl: "/admin",
-    });
-
-  if (status === "loading") {
-    return (
-      <div className="min-h-screen text-white flex items-center justify-center">
-        <div className="text-center">
-          <div className="inline-block animate-spin rounded-full h-16 w-16 border-y-2  mb-4"></div>
-          <h2 className="text-2xl font-semibold">
-            Laster inn administrasjonspanel...
-          </h2>
-          <p className="text-slate-400 mt-2">
-            Vennligst vent mens vi henter informasjonen din
-          </p>
-        </div>
-      </div>
-    );
-  }
-
-  if (!session) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <div className="flex flex-col items-center justify-center px-6 gap-5">
-          <h1 className="text-3xl">Vennligst logg inn</h1>
-          <Button
-            title="Logg inn med OW"
-            color="onlineOrange"
-            onClick={handleLogin}
-          />
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <Toaster />
-      {children}
-    </>
-  );
-};
 
 export const metadata: Metadata = {
   title: "Online Veldedighetsauksjon",
@@ -85,13 +31,13 @@ export default function WrappedRootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <SessionProvider>
+        <SessionWrapper>
           <RootLayout>
             <Navbar />
             <main className="flex-grow mt-28">{children}</main>
             <Footer />
           </RootLayout>
-        </SessionProvider>
+        </SessionWrapper>
       </body>
     </html>
   );
---
+++ components/auth/RootLayout.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { ReactNode } from "react";
+import { signIn, useSession } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+import { Button } from "@/components/Button";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  const { data: session, status } = useSession();
+
+  const handleLogin = () =>
+    signIn("auth0", {
+      callbackUrl: "/admin",
+    });
+
+  if (status === "loading") {
+    return (
+      <div className="min-h-screen text-white flex items-center justify-center">
+        <div className="text-center">
+          <div className="inline-block animate-spin rounded-full h-16 w-16 border-y-2  mb-4"></div>
+          <h2 className="text-2xl font-semibold">
+            Laster inn veldedighetsfesten...
+          </h2>
+          <p className="text-slate-400 mt-2">
+            Vennligst vent mens vi henter informasjonen din
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  if (!session) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center justify-center px-6 gap-5">
+          <h1 className="text-3xl">Vennligst logg inn</h1>
+          <Button
+            title="Logg inn med OW"
+            color="onlineOrange"
+            onClick={handleLogin}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
+};
+
+export default RootLayout;
