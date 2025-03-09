80064bcdd90e65779bd45f8aaac7a5ac69dbbe99
fredrir <fhansteen@gmail.com>
2025-03-09 15:02:22 +0100

    fix add prize goals

--- app/api/prize-goals/route.ts
+++ app/api/prize-goals/route.ts
@@ -4,20 +4,20 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/authOptions";
 
 export const GET = async () => {
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session.user) {
-    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-  }
-
   try {
-    const auctions = await prisma.auction.findMany();
+    const session = await getServerSession(authOptions);
 
-    if (!auctions) {
+    if (!session || !session.user) {
+      return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+    }
+
+    const prizeGoals = await prisma.prizeGoal.findMany();
+
+    if (!prizeGoals) {
       return NextResponse.json({ description: "Could not find any auctions" });
     }
 
-    return NextResponse.json(auctions);
+    return NextResponse.json(prizeGoals);
   } catch (error) {
     return NextResponse.json(
       { error: error instanceof Error ? error.message : "Unknown error" },
