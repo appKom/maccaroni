a85b7a643a4cfa24e42f4806b66b0470c5c8b0ae
fredrir <fhansteen@gmail.com>
2025-03-09 13:34:49 +0100

    feat: add markdown rulesheat

--- app/regelark/page.tsx
+++ app/regelark/page.tsx
@@ -1,19 +1,23 @@
-import Rulesheet from "@/components/Rulesheet";
+import ReactMarkdown from "react-markdown";
+import { prisma } from "@/lib/prisma";
+import rehypeRaw from "rehype-raw";
+import MarkdownComponents from "@/components/markdown/MarkdownComponents";
 
-export default function AuctionItemsPage() {
-  const data = {
-    rulesheet: {
-      id: 1,
-      title: "Regel 1",
-      description: "Beskrivelse 1",
-      _id: "1",
-      markdown: "Markdown content",
-    },
-  };
+export default async function AuctionItemsPage() {
+  const rules = await prisma.ruleSheet.findFirst();
+
+  if (!rules) {
+    return <p>{`Fant ingen regler :(`}</p>;
+  }
 
   return (
-    <div>
-      <Rulesheet rulesheet={data.rulesheet} />
+    <div className="flex flex-col items-start mx-auto max-w-4xl px-4">
+      <ReactMarkdown
+        rehypePlugins={[rehypeRaw]}
+        components={MarkdownComponents}
+      >
+        {rules.description}
+      </ReactMarkdown>
     </div>
   );
 }
