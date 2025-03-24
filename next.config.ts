eabe484a44fd968199536a03cab736a712b039e7
fredrir <fhansteen@gmail.com>
2025-03-24 18:08:55 +0100

    disable CORS for GET requests and upgrade next version

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["gjseonfnuqpkvgeqmfpt.supabase.co"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
